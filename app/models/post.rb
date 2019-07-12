76c00f9d79232b7fd2067f1fdd6403ef9b6d095a
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-12 17:28:16 +0200

    add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5, maximum: 20 }
   validates :body, presence: true, length: { minimum: 10 }
 end
